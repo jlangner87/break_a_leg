84cf75c06109a35b87e972b8d03111caf7c99898
Josh Langner <josh.langner@gmail.com>
2022-07-27 09:20:31 -0500

    base css styling and layout of app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,17 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>For the love of god, let this one thing work...</h1>
+      <div className="header">Home route here</div>
+      <h2 className="theater_name">Sometown Community Theater</h2>
+      <ul className="shows_container">
+        <li className="show_item">show here</li>
+        <li className="show_item">show here</li>
+        <li className="show_item">show here</li>
+        <li className="show_item">show here</li>
+        <li className="show_item">show here</li>
+        <li className="show_item">show here</li>
+      </ul>
+      <div className="footer">Admin login route here</div>
     </div>
   )
 }
