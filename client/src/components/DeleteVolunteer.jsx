182faa063050f78def92b800f84bb60362596021
Josh Langner <josh.langner@gmail.com>
2022-07-28 15:50:32 -0500

    all forms added to admin page. styled with required flexbox

---
+++ client/src/components/DeleteVolunteer.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
+const DeleteVolunteer = (props) => {
+  return (
+    <div>
+      <input type="text" name= "name" placeholder="enter name here"/>
+      <br></br>
+      <button className="submit">Submit</button>
+    </div>
+  )
+}
+
+  export default DeleteVolunteer
--- client/src/pages/AdminHome.js
+++ client/src/pages/AdminHome.js
@@ -2,19 +2,32 @@ import NewVolunteerForm from '../components/NewVolunteer'
 import NewShowForm from '../components/NewShow'
 import UpdateShow from '../components/UpdateShow'
 import UpdateVolunteer from '../components/UpdateVolunteer'
+import DeleteVolunteer from '../components/DeleteVolunteer'
 
 const AdminHome = () => {
   return (
     <div>
-      <div>
-        <h3 className="subheading">Add a volunteers</h3>
-        <NewVolunteerForm />
-        <h3 className="subheading">Add a shows</h3>
-        <NewShowForm />
-        <h3 className="subheading">Update a volunteer</h3>
-        <UpdateVolunteer />
-        <h3 className="subheading">Update a show</h3>
-        <UpdateShow />
+      <div className="form_container">
+        <form className="admin_form">
+          <h3 className="subheading">Add a volunteers</h3>
+          <NewVolunteerForm />
+        </form>
+        <form className="admin_form">
+          <h3 className="subheading">Add a shows</h3>
+          <NewShowForm />
+        </form>
+        <form className="admin_form">
+          <h3 className="subheading">Update a volunteer</h3>
+          <UpdateVolunteer />
+        </form>
+        <form className="admin_form">
+          <h3 className="subheading">Update a show</h3>
+          <UpdateShow />
+        </form>
+        <form className="admin_form">
+          <h3 className="subheading">Delete a volunteer</h3>
+          <DeleteVolunteer />
+        </form>
       </div>
     </div>
   )
