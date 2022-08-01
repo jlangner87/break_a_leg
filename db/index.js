349216b8aa3825f7aa75af8e612e8715060cc59d
Josh Langner <josh.langner@gmail.com>
2022-07-31 22:59:16 -0500

    heroku installed

--- db/index.js
+++ db/index.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/<database_name>'
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/theaterDatabase', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
+  .connect(dbUrl)
   .then(() => {
-    console.log('Theater Database successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
